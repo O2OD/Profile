1d443e1b3cf5286a7dd6a124266836c3662318e7
Ozodbek Tirkashov <tirkashovo03@gmail.com>
2026-08-20 11:41:09 +0500

    Token o'rniga Bearer qaytarildi

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -7,7 +7,7 @@ axios.interceptors.request.use(
   config => {
     const token = localStorage.getItem('access_token');
     if (token) {
-      config.headers['Authorization'] = 'Token ' + token;
+      config.headers['Authorization'] = 'Bearer ' + token;
     }
     return config;
   },
