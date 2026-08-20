113170de25a31bc8fdb11b8db25f06aee0348c26
Ozodbek Tirkashov <tirkashovo03@gmail.com>
2026-08-20 12:31:00 +0500

    done

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='insecure-secret-key-for-local-use')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*', 'profile-2nsm.onrender.com',]
